ba783637cd793807db968589acbb5428a1cf4a65
Marcus Nguyen <marcus.kimlong@gmail.com>
2023-10-13 05:25:37 +1000

    more hotfix

--- src/routes/ml_model.js
+++ src/routes/ml_model.js
@@ -1,9 +1,9 @@
 const express = require("express");
-
+import authorize from "../middleware/authorize.js";
 const router = express.Router();
 
 import * as controllers from "../controllers/ml_modelControllers.js";
 
-router.post("/predict", controllers.latePrediction);
+router.get("/predict", authorize(["user"]), controllers.latePrediction);
 
 module.exports = router;
